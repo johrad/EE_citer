d996310f19ce0f468d829259fa699bf71cb8722d
johrad <jpdragic@gmail.com>
2018-05-12 22:59:48 +0200

    commit 2

--- program.py
+++ program.py
@@ -4,6 +4,10 @@ import csv
 import datetime
 
 
+def open_csv():
+    with open('sources.csv', "rw") as csv_file:
+        pass
+
 def give_link():
     global link
     link = input("please give the link you\'d like to save\n> ")
@@ -13,9 +17,7 @@ def open_link(link):
     page = requests.get(link)
     print(page)
 
-def open_csv():
-    with open('sources.csv', newline="") as csv:
-        pass
+
 
 
 

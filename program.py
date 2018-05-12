76bec7ba32c494fcb0a8604ef995606e5e204e5e
johrad <jpdragic@gmail.com>
2018-05-12 20:37:44 +0200

    yes

--- program.py
+++ program.py
@@ -1,19 +1,30 @@
-import bs4
 from bs4 import BeautifulSoup
 import requests
 import csv
-import sys
+import datetime
 
-page = requests.get("https://docs.python.org/2/library/csv.html")
 
-print("HTTP {}".format(page))
+def give_link():
+    global link
+    link = input("please give the link you\'d like to save\n> ")
 
-def give_link() :
-    pass
 
-# def register current date():
+def open_link(link):
+    page = requests.get(link)
+    print(page)
+
+def open_csv():
+    with open('sources.csv', newline="") as csv:
+        pass
+
+
+
+running = True
+while running: 
+    give_link()
+    open_link(link)
+    running = False
+
 
 # def save_to_csv:
 #     dunno how to do this yet, read docs boi
-
-

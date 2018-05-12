bc6ddc88702647953afdab58f6ce4f0dda10d08d
johrad <jpdragic@gmail.com>
2018-05-12 20:10:57 +0200

    inital setup 2

--- program.py
+++ program.py
@@ -1,8 +1,19 @@
+import bs4
 from bs4 import BeautifulSoup
 import requests
 import csv
+import sys
 
+page = requests.get("https://docs.python.org/2/library/csv.html")
 
+print("HTTP {}".format(page))
 
+def give_link() :
+    pass
+
+# def register current date():
+
+# def save_to_csv:
+#     dunno how to do this yet, read docs boi
 
 
